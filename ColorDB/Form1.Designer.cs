f29320d8739015169da050ec399fa9b4599302b9
Opilane TTHK <opilane@tthk.ee>
2021-02-25 11:49:03 +0200

    Create Web page

---
+++ ColorDB/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ColorDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.PrimaryColor = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ColorType = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Colors";
+            // 
+            // PrimaryColor
+            // 
+            this.PrimaryColor.FormattingEnabled = true;
+            this.PrimaryColor.Location = new System.Drawing.Point(57, 133);
+            this.PrimaryColor.Name = "PrimaryColor";
+            this.PrimaryColor.Size = new System.Drawing.Size(93, 108);
+            this.PrimaryColor.TabIndex = 1;
+            this.PrimaryColor.SelectedIndexChanged += new System.EventHandler(this.PrimaryColor_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ColorType";
+            // 
+            // ColorType
+            // 
+            this.ColorType.FormattingEnabled = true;
+            this.ColorType.Location = new System.Drawing.Point(339, 130);
+            this.ColorType.Name = "ColorType";
+            this.ColorType.Size = new System.Drawing.Size(87, 108);
+            this.ColorType.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ColorType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PrimaryColor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox PrimaryColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox ColorType;
+    }
+}
+
---
+++ ColorDB/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorDB
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["ColorDB.Properties.Settings.ColorsConnectionString"].ConnectionString;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ColorTable();
+        }
+
+        private void ColorTable()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * From PrimaryColor", connection))
+            {
+                DataTable colorTable = new DataTable();
+                adapter.Fill(colorTable);
+
+                PrimaryColor.DisplayMember = "PrimaryColorName";
+                PrimaryColor.ValueMember = "Id";
+                PrimaryColor.DataSource = colorTable;
+            }
+        }
+
+        private void PrimaryColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ColorTypes();
+        }
+
+        private void ColorTypes()
+        {
+            string query = "SELECT PrimaryColor.Name FROM ColorId INNER JOIN Pet ON color.IdId = ColorId.Id WHERE PrimaryColor.Id =@TypeId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@TypeId", PrimaryColor.SelectedValue);
+                DataTable colorTable = new DataTable();
+                adapter.Fill(colorTable);
+                ColorType.DisplayMember = "Name";
+                ColorType.ValueMember = "Id";
+                ColorType.DataSource = colorTable;
+            }
+        }
+    }
+}
